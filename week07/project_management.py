511088bec09e50073c5a76bd7e4e56add401a0ff
jaskirat <jaskiratkaur1@my.jcu.edu.au>
2022-11-10 17:31:49 +1000

    adding main menu

--- week07/project_management.py
+++ week07/project_management.py
@@ -1,6 +1,9 @@
 from project import Project
+from operator import attrgetter
 
 # constants
+CONSTANT = "- (L)oad project\n-(S)ave project\n-(D)isplay project\n-(F)ilter project by date\n-(A)dd new project\n-(" \
+           "U)pdate project\n-(Q)uit "
 PROJECT_FILE = 'projects.texts'
 
 
@@ -36,10 +39,37 @@ def load_project(project_file):
 
 
 def main():
-    # test load_file
-    projects = load_project(PROJECT_FILE)
-    for project in projects:
-        print(project)
+    print(CONSTANT)
+    menu_choice = input(">>> ").upper()
+    while menu_choice != "Q":
+        if menu_choice == "L":
+            PROJECT_FILE = input("File name to load from:")
+            projects = load_project(PROJECT_FILE)
+        elif menu_choice == 's':
+            user_saved_file = input("File name to save to:")
+            out_file = open(user_saved_file, 'w')
+            for project in project:
+                print(repr(project), file=out_file)
+            out_file.close()
+        elif menu_choice == 'D':
+            projects.sort(key=attrgetter('priority'))
+            for project in projects:
+                print(project)
+        elif menu_choice == 'F':
+            print("Filter by date")
+        elif menu_choice == 'A':
+            name = input("Name:")
+            start_date = input("Start date (dd/mm/yyyy):")
+            priority = int(input("Priority:"))
+            cost_estimate = float(input("Cost estimate:"))
+            completion_percentage = int("Completion percentage:")
+            new_project = Project(name, start_date, priority, cost_estimate, completion_percentage)
+            projects.append(new_project)
+            print(new_project, "added to file.")
+        elif menu_choice == 'U':
+            project_to_update = input("Choose a project to update: ")
+        else:
+            print("Thank you for using custom-built project management software")
 
 
 if __name__ == '__main__':
